2715cce87a43ed903bdc213538645fba28e49dbb
Camile Costa <camilesilvadacosta@gmail.com>
2024-06-25 20:19:56 -0300

    Projeto inicial

---
+++ logica-js-jogo-adivinhacao/app.js
@@ -0,0 +1,34 @@
+alert('Bem-vindo ao jogo do número secreto');
+let numeroMaximo = 10;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto){
+    chute = prompt(`Escreva um número de 1 a ${numeroMaximo}`);
+    // se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert (`O número secreto é menor que ${chute}`);
+        } else {
+            alert (`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
+}
+
+// operador tenário = tem a mesma função do if = tentativa > 1 ? Faã isso se não faça aquilo
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+
+//if (tentativas > 1) {
+//    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//} else {
+//    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//}
